967e2d814cb66f09c7011edfda582f893886e683
Garrett Keyes <garrett.keyes@gmail.com>
2025-11-10 20:35:58 -0500

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Garrett Keyes on 11/10/25.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
